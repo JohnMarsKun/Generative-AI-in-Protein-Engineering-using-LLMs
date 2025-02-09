bae1e3fc65471c4d8898496301fe459342b73643
John Maris <88288021+JohnMarsKun@users.noreply.github.com>
2025-02-09 22:14:42 +0100

    Delete PLMs directory

--- PLMs/src/__init__.py
+++
@@ -1,5 +0,0 @@
-from .esm_model import ESM2, ESM2EncoderModel, ProteinModel
-from .configurations import create_config, get_bert_config
-from .interpolations import linear_interpolation, sinusoidal_interpolation, arccos_interpolation, decode_interpolations
-from .utils import levenshtein_distance, generate_random_sequence
-from .visualization import plot_umap_2d, plot_umap_3d
